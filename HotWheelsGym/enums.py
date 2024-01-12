dbcf028864d49b4c9d5d973dbffcd117881e76d9
zackbeucler <zbeucler@gmail.com>
2024-01-12 09:26:16 -0500

    wip adding more tracks

--- HotWheelsGym/enums.py
+++ HotWheelsGym/enums.py
@@ -8,6 +8,13 @@ class Tracks(enum.Enum):
 
     TRex_Valley = "trex_valley"
     Dino_Boneyard = "dino_boneyard"
+    Monsters_of_the_Deep = "monsters_of_the_deep"
+    Whiteskull_Cliffs = "whiteskull_cliffs"
+    Jungle_Snakepit = "jungle_snakepit"
+    Gator_Forest = "gator_forest"
+    Solar_Strip = "solar_strip"
+    Satellite_Mission = "satellite_mission"
+    
 
 
 class RaceMode(enum.Enum):
